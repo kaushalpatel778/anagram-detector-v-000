cadee6eab0fbfc477577133ec88ceed7833a4409
Kaushal Patel <kaushalpatel778@gmail.com>
2018-05-02 03:36:07 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,13 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word
+  @store = []
+  def initialize(word)
+    @word = word
+  end
+  def match(sentence)
+    @store << sentence
+    
+  end
+
+end
